0afc166c64c2089a09e97f35a1d7cce8501147c7
Subin Lee <112736264+subillie@users.noreply.github.com>
2024-11-20 11:16:10 +0900

    Chore: urls에 ReportView 추가

--- navigation/urls.py
+++ navigation/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import FindPathView, CallTmapView,PedestrianRouteView
+from .views import FindPathView, CallTmapView, PedestrianRouteView, ReportView
 
 app_name = "navigation"
 
 urlpatterns = [
     path("find_path/", FindPathView.as_view(), name="find_path"),
     path("call_tmap/", CallTmapView.as_view(), name="call_tmap"),
-    path('pedestrian-route/', PedestrianRouteView.as_view(), name='pedestrian-route'),
+    path("pedestrian-route/", PedestrianRouteView.as_view(), name="pedestrian-route"),
+    path("report/", ReportView.as_view(), name = "report")
 ]
