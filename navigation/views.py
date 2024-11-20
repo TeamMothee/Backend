19f5b025b659bb6a81be198f302c384bb7f51288
Subin Lee <112736264+subillie@users.noreply.github.com>
2024-11-20 11:14:38 +0900

    Chore: 삭제된 ReportView 롤백

--- navigation/views.py
+++ navigation/views.py
@@ -197,3 +197,46 @@ class PedestrianRouteView(APIView):
 
         # TMap API 응답 그대로 반환
         return Response(optimal_route_response, status=status.HTTP_200_OK)
+
+
+class ReportView(APIView):
+    @swagger_auto_schema(
+        operation_summary="위험 구조물 제보",
+        operation_description="Report a dangerous structure",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "image": openapi.Schema(type=openapi.TYPE_STRING),
+                "latitude": openapi.Schema(type=openapi.TYPE_NUMBER),
+                "longitude": openapi.Schema(type=openapi.TYPE_NUMBER),
+            },
+        ),
+        responses={
+            200: "OK",
+            400: "Invalid input arguments",
+        },
+    )
+    def patch(self, request, *args, **kwargs):
+        try:
+            image = request.data["image"]
+            latitude = request.data["latitude"]
+            longitude = request.data["longitude"]
+        except KeyError:
+            return Response(
+                {"error": "Invalid input arguments"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        Detection.create_table(image, latitude, longitude)
+        # AI 모델로 위험 구조물 탐지
+        # road_structure = detect_danger(image)
+        road_structure = {  # 임시로 작성함
+            "braille_block": 0,
+            "audio_signal": 1,
+            "bollard": 2,
+            "weight": 10,
+            "latitude": latitude,
+            "longitude": longitude,
+        }
+        RoadStructure.create_table(**road_structure)
+        Detection.delete_table(latitude, longitude)
+
+        return Response(status=status.HTTP_200_OK)
