d19b9c199a906a174e89ceb1d4882b157aa8f516
acupoframen <choe.hwan@gmail.com>
2024-12-04 15:54:01 +0900

    fix: api_key naming error 해결: dictionary 형식으로 변경

--- navigation/function.py
+++ navigation/function.py
@@ -57,11 +57,7 @@ def calculate_path_response(start, end, passList=None):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-<<<<<<< HEAD
         "appKey": TMAP['APP_KEY'],
-=======
-        "appKey": TMAP["APP_KEY"],
->>>>>>> main
     }
 
     payload = {
